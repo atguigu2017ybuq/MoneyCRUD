9616394db9627afea6661b6224355a0eaee359fa
fengjie_global <fengjie_global@atguigu.com>
2017-08-01 09:46:02 +0800

    push to remote

--- src/main/java/com/atguigu/git/money/Person.java
+++ src/main/java/com/atguigu/git/money/Person.java
@@ -5,6 +5,7 @@ public class Person {
 	public static void main(String[] args) {
 		System.out.println("I am a person ...");
 		System.out.println("I am a person edit by ybuq ...");
+		System.out.println("I am a person edit by lhuc ...");
 	}
 
 }
--- src/main/java/com/atguigu/git/money/User.java
+++ src/main/java/com/atguigu/git/money/User.java
@@ -1,5 +1,9 @@
 package com.atguigu.git.money;
 
 public class User {
+	
+	public static void main(String[] args) {
+		System.out.println("I am a person ...");
+	}
 
 }
